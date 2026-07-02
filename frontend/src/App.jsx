57ff0f527b902c387ffaf215bca7ce86d43f3471
MT Rashid <mt.rashid.dev@gmail.com>
2026-07-02 12:55:03 +0600

    add component Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/utility/layout/Layout.jsx";
+import Home from "./components/home/Home.jsx";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<Layout>
-            <div className={"text-success"}>This is home page</div>
+            <Home/>
           </Layout>}/>
         </Routes>
       </BrowserRouter>
